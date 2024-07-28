6091b33a38463fa1e59a5f0a77affbfe611e9b96
Daniil-svg814 <abduhalikovdaniil@gmail.com>
2024-07-28 23:22:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# '1st program'
+print(9 ** 0.5 * 5)
+# '2st program'
+print(9.99 > 9.98 and 1000 < 1000.1)
+# '3st program'
+print(1234 % 1000, 234 // 10)
+print(5678 % 1000, 678 // 10)
+print(23 + 67)
